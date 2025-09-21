77feacca0db5d24a84ca5bc1865f799907bbcff4
Martu Peluso <martupeluso@proton.me>
2025-09-20 22:54:13 -0300

    Create constructor for books

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const books = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID(); // generates a unique id, preventing issues when books get removed
+}
